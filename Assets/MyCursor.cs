c34aa27222488be9b04a2cef1495bf86c3e44d89
aradl <aradl@mitre.org>
2015-04-27 11:01:32 -0400

    initial commit with good gitignore

---
+++ Assets/MyCursor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyCursor : MonoBehaviour {
+
+	public GameObject CurrentObject;
+	public Camera _camera;
+
+	// Use this for initialization
+	void Start () {
+		_camera = this.gameObject.GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (CurrentObject != null)
+			CurrentObject.transform.position = _camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
+		if (Input.GetMouseButtonDown (0) && CurrentObject)
+			CurrentObject = null;
+	}
+}
---
+++ Assets/grid_items.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class grid_items : MonoBehaviour {
+
+	public GameObject buttonPrefab;
+	public Transform layoutPanel;
+
+	public string ItemFolderPath = "";
+
+	private GameObject[] _prefabs;
+	// Use this for initialization
+	void Start () {
+		createGrid (ItemFolderPath);
+	}
+
+	public void createGrid(string path)
+	{
+		_prefabs = Resources.LoadAll<GameObject> (path);
+		for(int i = 0; i < _prefabs.Length; i++) {
+			int crappyscopehack = i;
+			GameObject b = (GameObject)Instantiate(buttonPrefab);
+			Texture2D t = AssetPreview.GetAssetPreview(_prefabs[i]);
+			b.GetComponentInChildren<Image>().sprite = Sprite.Create(t, new Rect(0,0,t.width,t.height), new Vector2(0.5f,0.5f));
+			b.transform.SetParent(layoutPanel, false);
+
+			Button bu = b.GetComponent<Button>();
+			bu.onClick.AddListener(() => genPrefab(crappyscopehack));
+		}
+	}
+
+	private void genPrefab(int index)
+	{
+		GameObject go = Instantiate (_prefabs[index]);
+		//call cursor script and hand the prefab to it
+		MyCursor c = (MyCursor)GameObject.Find ("Main Camera").GetComponent (typeof(MyCursor));
+		c.CurrentObject = go;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
